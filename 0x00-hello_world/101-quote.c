0ec0f337cdc8d90c662b42886340bca9d5006068
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-16 01:26:09 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char v[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char v[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, v, sizeof(v) - 1);
 return (1);
 }
