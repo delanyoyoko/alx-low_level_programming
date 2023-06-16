073db5acbb00e60196b5f60deadbebca36930179
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-16 01:00:21 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,5 @@ printf("Size of long long: %zu byte(s)\n", sizeof(long long));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
 printf("Size of double: %zu byte(s)\n", sizeof(double));
 printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-
 return (0);
 }
