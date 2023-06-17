afd318232bb51ca61e8f0baddf8b2102d5b76fa4
delanyo agbenyo <delanyodevio@gmail.com>
2023-06-17 13:17:24 -0700

    Statsh

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ last = n % 10;
 /* your code goes there */
 if (last == 0)
 {
-  printf("Last digit of %d is %d and is 0\n", n, last);
+ printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else if (last < 6)
 {
-  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
 else if (last > 5)
 {
