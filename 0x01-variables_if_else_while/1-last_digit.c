743e0270495cb51ddffb2c7b0ee9ad824d475fb7
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-17 21:09:47 +0000

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ last = n % 10;
  */
 if (n == 0)
 {
-printf("Last digit of %d is %d and is 0", n, last);
+printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else if (n < 6)
 {
-printf("Last digit of %d is %d and is less than and not 0", n, last);
+printf("Last digit of %d is %d and is less than and not 0\n", n, last);
 }
 else if (n > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, last);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
 return (0);
 }
