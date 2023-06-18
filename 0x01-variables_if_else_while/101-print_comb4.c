f183af0fdc9f3d7af2da756412d3b97e6e8fb756
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-18 17:23:34 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,16 +7,26 @@
  */
 int main(void)
 {
-int number;
-
-for (number = 0; number < 10; number++)
+int i;
+int j;
+int k;
+  
+for (i = 0; i <= 7; i++)
 {
-putchar(number + '0');
-
-if (number != 9)
+for (j = i + 1; j <= 8; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+  
+if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 
