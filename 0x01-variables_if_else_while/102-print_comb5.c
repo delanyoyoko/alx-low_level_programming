8770f0857e484800724d1204f260c71e495725d1
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-18 17:33:59 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 int i;
 int j;
 
-for (i = 0; i < 99; i++)
+for (i = 0; i < 100; i++)
 {
 for (j = i; j < 100; j++)
 {
@@ -20,7 +20,7 @@ putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
 
-if (i != 98 || j != 99)
+if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
