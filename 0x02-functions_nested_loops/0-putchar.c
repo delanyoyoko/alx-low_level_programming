ed98e55fbc6cbf32f9230de95dde2341a6cc4211
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-21 03:22:50 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * main - Entry point
@@ -10,23 +8,6 @@
  */
 int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else
-{
-printf("%d is zero\n", n);
-}
+printf("_putchar\n");
 return (0);
 }
