ccc9a35f22479dfb4d7e8a4b953d90ca59174c32
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-23 23:34:52 +0000

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return 1;  // Uppercase character
-    }
-    return 0;  // Not an uppercase character
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return 1;  
+}
+return 0; 
 }
