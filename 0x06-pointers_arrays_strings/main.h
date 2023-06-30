50cb430c06da8edc27bb54daf5e59cd03aa20c05
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-06-30 00:27:36 +0000

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,28 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
+
+
+char *_strcat(char *dest, char *src)
+{
+char *ptr = dest;
+
+// Find the end of the dest string
+while (*ptr != '\0')
+{
+ptr++;
+}
+
+// Append the src string to dest
+while (*src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+}
+
+// Add the terminating null byte
+*ptr = '\0';
+
+return dest;
+}
