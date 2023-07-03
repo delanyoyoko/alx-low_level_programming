80d3ccef7aae0f8a8f0d92570edf7e643f4cb41c
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-03 18:20:37 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -20,18 +20,3 @@ i++;
 }
 return s;
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char buffer[10];
-char constant = 'A';
-
-_memset(buffer, constant, sizeof(buffer));
-printf("Buffer: %s\n", buffer);
-return 0;
-}
