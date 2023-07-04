c38e71d9e9795fd4f6dd8e62d3e51f7501fced76
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-04 13:23:54 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,9 +8,9 @@
  *
  * Return: Nothing.
  */
-char* _memset(char* s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-char* p = s;
+char *p = s;
 unsigned int i = 0;
 
 while (i < n)
