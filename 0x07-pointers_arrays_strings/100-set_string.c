2e4b3b4c3c3432782bcf84b024eed7a9ada44de3
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-04 16:59:18 +0000

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: a double pointer of string
+ * @to: a pointer of string
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+size_t len = strlen(to) + 1;
+char* temp = realloc(*s, len);
+
+if (temp == NULL)
+{
+printf("Memory allocation failed.\n");
+return;
+}
+
+*s = temp;
+strcpy(*s, to);
+}
