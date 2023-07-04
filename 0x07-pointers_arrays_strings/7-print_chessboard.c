336f435048a752e4c70bfb5887591861cbbfbc38
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-04 16:25:31 +0000

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_chessboard - prints a chessboard
@@ -16,10 +17,10 @@ int j = 0;
 
 while (j < 8)
 {
-printf("%c ", a[i][j]);
+_putchar("%c ", a[i][j]);
 j++;
 }
-printf("\n");
+_putchar("\n");
 i++;
 }
 }
