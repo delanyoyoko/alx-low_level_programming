3151ff08bce07c0593541e9f8c2e3c9bfd6587c9
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-04 03:11:39 +0000

    Update 0-main.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,23 +1,50 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+unsigned int i;
+
+i = 0;
+
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+
+printf("0x%02x", buffer[i]);
+
+i++;
+}
+printf("\n");
+}
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-char buffer[10];
-char constant = 'A';
+char buffer[98] = {0x00};
 
-_memset(buffer, constant, sizeof(buffer));
-printf("Buffer: %s\n", buffer);
-return 0;
+simple_print_buffer(buffer, 98);
+_memset(buffer, 0x01, 95);
+printf("-------------------------------------------------\n");
+simple_print_buffer(buffer, 98);
+return (0);
 }
