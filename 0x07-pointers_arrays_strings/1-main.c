deab0889dd20780c45b362a10161c79808e34898
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-04 14:52:28 +0000

    Update 1-main.c

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -1,4 +1,3 @@
-??? from here until ???END lines may have been inserted/deleted
 #include "main.h"
 #include <stdio.h>
 
@@ -11,23 +10,24 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+i = 0;
+while (i < size)
+{
+if (i % 10)
+{
+printf(" ");
+}
+
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
@@ -37,10 +37,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+char buffer[98] = {0};
+char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf
-???END
+simple_print_buffer(buffer, 98);
+_memcpy(buffer + 50, buffer2, 10);
+printf("-------------------------------------------------\n");
+simple_print_buffer(buffer, 98);
+return (0);
+}
