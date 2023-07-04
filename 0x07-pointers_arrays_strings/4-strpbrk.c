e2d948d11a05349b95d14218f75abc2993a67261
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-04 15:46:28 +0000

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,7 +6,7 @@
  * @s: string to search.
  * @accept: the predicate.
  *
- * Return: Pointer to the byte in s matches one of the bytes in accept
+ * Return: Pointer to the byte in s that matches one of the bytes in accept
  */
 char *_strpbrk(char *s, char *accept)
 {
