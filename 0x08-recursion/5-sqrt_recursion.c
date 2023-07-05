4688572a522e6ca0354855833b3b18530e112b02
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-05 19:33:27 +0000

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,49 @@
-int _sqrt_recursion(int n) {
-    if (n < 0) {
-        return -1; // Error: negative number
-    }
-    else if (n == 0 || n == 1) {
-        return n; // Base case: square root of 0 or 1 is the number itself
-    }
-    else {
-        return sqrt_recursive(n, 1, n); // Initial recursive call with search space [1, n]
-    }
+/**
+ * _sqrt_recursion_helper - performs the actual recursive calculation
+ * @n: an integer
+ * @start: the start index
+ * @end: the end index
+ *
+ * Return: Natural square root of a number or Error
+ */
+int _sqrt_recursion_helper(int n, int start, int end)
+{
+if (start > end)
+{
+return (-1);
+}
+
+int mid = (start + end) / 2;
+int square = mid * mid;
+
+if (square == n)
+{
+return (mid);
+}
+
+if (square < n)
+{
+return (_sqrt_recursion_helper(n, mid + 1, end));
+}
+return (_sqrt_recursion_helper(n, start, mid - 1));
+}
+
+/**
+ * _sqrt_recursion - determines the value of x raised to the power of y
+ * @n: an integer
+ *
+ * Return: Natural square root of a number, or Error
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+
+if (n == 0 || n == 1)
+{
+return (n);
+}
+return (_sqrt_recursion_helper(n, 0, n));
 }
