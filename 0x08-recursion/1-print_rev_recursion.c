44af63ab9681f31dac4437c72ebec4a9f23c1af3
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-05 18:18:05 +0000

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * recursively
+ * @s: a character pointer
+ *
+ * Return: Nothing.
+ */
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
