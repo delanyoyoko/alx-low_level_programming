0dc7f87ee23fc76ff080ba93c4a823f3573e1768
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-05 18:27:08 +0000

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen_recursion - determines length of a string
+ * recursively
+ * @s: a character pointer
+ *
+ * Return: Length of a string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+return 1 + _strlen_recursion(s + 1);
+}
