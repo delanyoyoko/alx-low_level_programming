9c60e369a266476f71c9038c8bfcbd808d28de83
delanyo agbenyo <delanyodevio@gmail.com>
2023-07-08 07:34:02 -0700

    Added second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+if (argv)
+{
+printf("%d\n", argc - 1);
+}
+return (0);
+}
