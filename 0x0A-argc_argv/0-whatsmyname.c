d526952f8f074fcebeb09bc008f7953871d18ae5
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-08 14:02:27 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,11 @@
 
 /**
  * main - prints its name
- * @argc: argument count
  * @argv: argument vector
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
