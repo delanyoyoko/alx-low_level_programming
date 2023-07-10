5ecd5103bb07e410b792c1838766c21c3b0c4d50
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-07-10 15:04:46 +0000

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * create_array - creates an array of chars, 
+ * create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: the size of the memory to print
  * @c: the address of memory to print
