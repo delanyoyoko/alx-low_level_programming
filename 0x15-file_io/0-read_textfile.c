ca5f515f2cc7113d66993633caaf2125ef99ccd1
delanyo agbenyo <73528412+delanyoyoko@users.noreply.github.com>
2023-08-07 16:53:28 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,14 +15,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t file_desc;
 	ssize_t text_mode;
 	ssize_t write_mode;
-	
+
 	file_desc = open(filename, O_RDONLY);
 	if (file_desc == -1)
 		return (0);
 	buff_mode = malloc(sizeof(char) * letters);
 	text_mode = read(file_desc, buff_mode, letters);
 	write_mode = write(STDOUT_FILENO, buff_mode, text_mode);
-	
+
 	free(buff_mode);
 	close(file_desc);
 	return (write_mode);
